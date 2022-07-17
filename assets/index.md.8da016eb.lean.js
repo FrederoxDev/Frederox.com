5158ecd7017e3ed9cb1f80988319104b070e5887
FrederoxDev <FrederoxDev@users.noreply.github.com>
2022-07-17 15:03:49 +0000

    deploy: 9e825da60a83b2a9c27127482bf5730a033791c4

---
+++ assets/index.md.8da016eb.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a,a as n}from"./app.b4e6c833.js";const x=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"index.md"}'),o={name:"index.md"},s=n("p",null,"Index",-1),r=[s];function c(d,_,i,p,l,m){return a(),t("div",null,r)}var u=e(o,[["render",c]]);export{x as __pageData,u as default};
